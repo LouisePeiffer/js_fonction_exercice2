734adcf4b1fe51970e37874664a0be91f1f4b4f2
Louise <louisepeiffer@hotmail.com>
2021-01-13 16:32:50 +0100

    exo 1 à 3

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+// Exo 1
+// let reverseNumber = (num) => {
+//     return num.toString().split("").reverse().join("")
+// }
+
+// console.log(reverseNumber(12345));
+
+// Exo 2
+// let divPar2 = (a) => {
+//     switch (a % 2 === 0) {
+//         case true:
+//             return "Le num x est divisible par 2"
+//         default: 
+//             return "Ce nombre n'est pas divisible par 2"
+//     }
+// }
+// console.log(divPar2(5))
+
+// Exo 3
+// let logIn = () => {
+//     let mdp = "badb1030"
+//     let quest = prompt('Mot de passe?')
+//         if (quest == mdp) {
+//             alert("Vous êtes connecté")
+//         } else {
+//             do {
+//                 quest2 = prompt("Essayez un autre mot de passe")
+//             } while (quest2 != mdp);
+//             alert('Bien joué')
+//     }
+// }
+// console.log(logIn());
+
+// Exo 4
+
